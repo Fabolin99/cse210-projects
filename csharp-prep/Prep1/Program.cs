ef0eb5282e1b63e159e156638f1cbc94f67d85fd
fabolin99 <fabdiaz99@gmail.com>
2023-01-18 23:27:50 -0700

    new

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,14 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("what is your first name?");
-        string first = Console.ReadLine();
-        Console.WriteLine("What is your last name?");
-        string second = Console.ReadLine();
+        // Console.WriteLine("what is your first name?");
+        // string first = Console.ReadLine();
+        // Console.WriteLine("What is your last name?");
+        // string second = Console.ReadLine();
 
     
 
-        Console.WriteLine($"Your name is {second}, {first} {second}.");
+        // Console.WriteLine($"Your name is {second}, {first} {second}.");
+
+
+    // practice
+
+    Introduction();
+    PromptAnswer("Do you consider yourself important? (A (strongly agree)/ B (Somewhat agree)/ C(I disagree):)");
 
     }
+    static void Introduction()
+    {
+        Console.WriteLine("Welcome to the program");
+        Console.WriteLine("Please, read carefully!");
+        Console.WriteLine("This program helps you choose your new hobbie according to your answers");
+        Console.WriteLine("Please, be honest! The answers are private");
+    }
+    int Count = 0;
+    static int PromptAnswer(int Count)
+    {
+        int A = 1;
+        int B = 2;
+        int C = 3;
+        int D = 4;
+
+        
+    }
+
 }
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Jobs job1 = new Jobs();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        //job1.DisplayJobDetails();
+
+        Jobs job2 = new Jobs();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        //job2.DisplayJobDetails();
+        Resume resume1 = new Resume();
+        resume1._name = "Fabian Diaz Santiago";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+        
+        resume1.DisplayResume();
     }
 }
