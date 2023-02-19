2eb0b7a950375112727cfc416b28728c95bfa0ad
fabolin99 <fabdiaz99@gmail.com>
2023-02-18 22:45:24 -0700

    new

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        string answer = "";
+        Reference information = new Reference("1 Corinthians", "13", "4");
+        information.displayInformation();
+        Scripture bible = new Scripture("Charity suffereth long, and is kind; charity envieth not; charity vaunteth not itself, is not puffed up"); 
+
+        while(answer != "quit")
+        {
+            bible.displayScripture();
+            answer = Console.ReadLine();
+            bible.hideRandomWord();
+            Console.Clear();
+            Console.WriteLine("Please press enter to hide words or type 'quit' to finish");
+            Console.WriteLine();
+            
+        }
+        
+        
+
     }
 }
