86cdb2d4025d68f45b79dc4d173fed3ff1e2d455
fabolin99 <fabdiaz99@gmail.com>
2023-03-07 00:37:48 -0700

    new

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,25 @@
 using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square(3,"Red");
+        shapes.Add(s1);
+
+        Rectangle s2 = new Rectangle(4, 5,"Blue");
+        shapes.Add(s2);
+
+        Circle s3 = new Circle(6,"Green");
+        shapes.Add(s3);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
-}
+}   
